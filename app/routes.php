f00942c5a4b18b50c38fb8854f6f0eaea804c1ab
SHUBHOMOY BISWAS <biswas_shubhomoy777@yahoo.com>
2016-01-21 19:58:39 +0530

    new route

--- app/routes.php
+++ app/routes.php
@@ -12,10 +12,6 @@ App::missing(function($exception) {
 Route::get('/api/search/doctors', 'DoctorController@search');
 
 Route::get('/api/specializations', 'DoctorController@showAllSpecializations');
-Route::get('/api/doctor/{id}', 'DoctorController@show');
-Route::get('/api/doctor/{id}/appointments', 'DoctorController@showAppointments');
-Route::get('/api/doctor/{doctor_id}/clinic/{clinic_id}', 'DoctorController@addRemoveClinic');
-Route::get('/api/doctor/{doctor_id}/specialization/{spec_id}', 'DoctorController@addRemoveSpecialization');
 Route::post('/auth/doctor/signup', 'DoctorController@signup');
 Route::post('/auth/doctor/login', 'DoctorController@login');
 
--- app/routes/api.php
+++ app/routes/api.php
@@ -2,3 +2,7 @@
 Route::get('/doctor/{id}', 'DoctorController@show');
 Route::get('/clinic/{id}', 'ClinicController@show');
 Route::get('/search/doctors', 'DoctorController@search');
+Route::get('/doctor/{id}', 'DoctorController@show');
+Route::get('/doctor/{id}/appointments', 'DoctorController@showAppointments');
+Route::get('/doctor/{doctor_id}/clinic/{clinic_id}', 'DoctorController@addRemoveClinic');
+Route::get('/doctor/{doctor_id}/specialization/{spec_id}', 'DoctorController@addRemoveSpecialization');
